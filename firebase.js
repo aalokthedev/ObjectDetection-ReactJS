b3a69b49bc243470b7f24740710bb79fc1f1dedd
Aalok Jha <aalokthedev@gmail.com>
2021-05-23 17:32:40 +0545

    First commit

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD6fyc4g2xuX9l761nSCN5JVebeVFzAmPA",
+    authDomain: "objdetect-aalok.firebaseapp.com",
+    projectId: "objdetect-aalok",
+    storageBucket: "objdetect-aalok.appspot.com",
+    messagingSenderId: "409096937080",
+    appId: "1:409096937080:web:9f5ed528e47c07d3fc178d",
+    measurementId: "G-X4V2GX3TB8"
+  };
